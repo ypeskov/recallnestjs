dd1bf6ade41a10b0de40a256bb6f4b15a672fb8a
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-05-15 11:12:13 +0300

    ff

--- recall/dist/cats/cats.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cat } from './cat.interface';
-import { CatsService } from './cats.service';
-import { CreateCatDto } from './create-cat.dto';
-export declare class CatsController {
-    private catsService;
-    constructor(catsService: CatsService);
-    create(createCatDto: CreateCatDto): Promise<void>;
-    findAll(): Promise<Cat[]>;
-}
